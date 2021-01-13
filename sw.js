96afd044fa7aef5e84de8e07497049d1499fa44c
Rafael de Jesus Saraiva <rafajs7@gmail.com>
2021-01-13 23:37:38 +0000

    sw update

--- sw.js
+++ sw.js
@@ -12,11 +12,11 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
-      './index.html',
-      './media',
-      './js',
-      './fonts',
-      './css'
+      './ccdm-uc/index.html',
+      './ccdm-uc/media',
+      './ccdm-uc/js',
+      './ccdm-uc/fonts',
+      './ccdm-uc/css'
     ]);
   });
 }
